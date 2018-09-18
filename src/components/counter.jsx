2c3cdafe83acd1b856f6674c25eb9c2f0eb70999
Fatayo <temitope.fatayo@cgi.com>
2018-09-18 16:37:02 -0300

    updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <div>
         <span className={this.getBadgetClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.renderTags()}
       </div>
     );
   }
